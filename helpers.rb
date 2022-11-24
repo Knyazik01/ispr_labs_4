44018180b296ced6f8ddb065a2163334b0d458dc
Knyazik01 <Knyazik01>
2022-11-24 22:06:40 +0200

    Lab 4
    change answers output to hash
    move functions to helpers

--- helpers.rb
+++ helpers.rb
@@ -1,13 +1,13 @@
-def get_random_answer (count = 1)
-  result = []
-  count.times {result.push %w[Так Ні].sample}
+def get_random_answer (questions)
+  result = {}
+  questions.each { |question| result[question] = %w[Так Ні].sample }
   result
 end
 
 def generate_answers_hash (hash)
   result = {}
   hash.entries.each do |key, questions_by_temperament|
-    result[key] = get_random_answer(questions_by_temperament.length)
+    result[key] = get_random_answer(questions_by_temperament)
   end
   result
 end
@@ -15,4 +15,48 @@ end
 def generate_answers (hash, filename = 'answers')
   answers = generate_answers_hash hash
   File.write("./json/#{filename}.json", JSON.pretty_generate(answers))
+end
+
+def get_answer (question)
+  puts question
+  answer = gets.chomp.downcase
+  is_valid = %w[так ні].include?(answer.downcase)
+  if is_valid
+    answer
+  else
+    puts "Введіть так або ні"
+    get_answer question
+  end
+end
+
+def get_answers (questions_hash)
+  # select answer mode
+  puts "Оберіть тип надання відповідей:\n1. Вручну\n2. У файлі за шляхом ./json/answers.json"
+  answer_mode = gets.chomp.to_i
+  if answer_mode === 1
+    answers = {}
+    puts "Надайте відповіді на запитання"
+    questions_hash.entries.each_with_index do |entries, index|
+      section, section_questions = entries
+      puts "Секція #{index + 1}"
+      section_questions.each do |current_question|
+        answer = get_answer current_question
+        if answers[section].kind_of?(Hash)
+          answers[section][current_question] = answer
+        else
+          answers[section] = {}
+          answers[section][current_question] = answer
+        end
+      end
+    end
+    # save user answers in file
+    File.write("./json/answers.json", JSON.pretty_generate(answers))
+    answers
+  elsif answer_mode === 2
+    answers_file = File.read('./json/answers.json')
+    JSON.parse(answers_file)
+  else
+    puts "Оберіть правильний тип"
+    get_answers questions_hash
+  end
 end
--- main.rb
+++ main.rb
@@ -1,49 +1,6 @@
 require 'json'
 require './helpers'
 
-def get_answer (question)
-  puts question
-  answer = gets.chomp.downcase
-  is_valid = %w[так ні].include?(answer.downcase)
-  if is_valid
-    answer
-  else
-    puts "Введіть так або ні"
-    get_answer question
-  end
-end
-
-def get_answers (questions_hash)
-  # select answer mode
-  puts "Оберіть тип надання відповідей:\n1. Вручну\n2. У файлі за шляхом ./json/answers.json"
-  answer_mode = gets.chomp.to_i
-  if answer_mode === 1
-    answers = {}
-    puts "Надайте відповіді на запитання"
-    questions_hash.entries.each_with_index do |entries, index|
-      section, section_questions = entries
-      puts "Секція #{index + 1}"
-      section_questions.each do |current_question|
-        answer = get_answer current_question
-        if answers[section].kind_of?(Array)
-          answers[section].push answer
-        else
-          answers[section] = [answer]
-        end
-      end
-    end
-    # save user answers in file
-    File.write("./json/answers.json", JSON.pretty_generate(answers))
-    answers
-  elsif answer_mode === 2
-    answers_file = File.read('./json/answers.json')
-    JSON.parse(answers_file)
-  else
-    puts "Оберіть правильний тип"
-    get_answers questions_hash
-  end
-end
-
 # read questions
 questions_file = File.read('./json/questions.json')
 questions = JSON.parse(questions_file)
@@ -58,7 +15,7 @@ answers = get_answers questions
 # calc yes answers
 yes_by_section = {}
 answers.entries.each do |temperament, temperament_answers|
-  positive_count = temperament_answers.select { |answer| answer.chomp.downcase === 'так' }.count
+  positive_count = temperament_answers.values.select { |answer| answer.chomp.downcase === 'так' }.count
   yes_by_section[temperament] = positive_count
 end
 
@@ -75,10 +32,10 @@ end
 
 # log results
 temperament_names = {
-"choleric" => "холерик",
-"melancholic" => "сангвінік",
-"phlegmatic" => "флегматик",
-"sanguine" => "меланхолік",
+"section_1" => "холерик",
+"section_2" => "сангвінік",
+"section_3" => "флегматик",
+"section_4" => "меланхолік",
 }
 
 name_len = (temperament_names.max_by {|_key, name| name.length})[1].length
